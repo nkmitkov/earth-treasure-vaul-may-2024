56940873d6b13fbfea0061efd5cd2eb34e68067c
Nick <nkmitkov@gmail.com>
2024-06-11 10:36:13 +0300

    Add mongoose database

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const routes = require("./routes");
 const configExpress = require("./config/configExpress");
@@ -12,4 +13,10 @@ configHandlebars(app);
 
 app.use(routes);
 
-app.listen(port, () => console.log("Server is listening on port 3000..."));
+const connectionString = "mongodb://localhost:27017/earth-treasure-vaul";
+mongoose.connect(connectionString)
+    .then(() => {
+        console.log("DB connected");
+        app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+    })
+    .catch((err) => console.log(`DB connection error -> ${err}`));
