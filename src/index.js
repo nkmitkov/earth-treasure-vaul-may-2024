733900d89601cfebf16569d1bbe0c3ac80a0e3a8
Nick <nkmitkov@gmail.com>
2024-06-11 08:48:52 +0300

    Add router and show home page

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 
+const routes = require("./routes");
 const configExpress = require("./config/configExpress");
 const configHandlebars = require("./config/configHandlebars");
 
@@ -9,8 +10,6 @@ const port = 3000;
 configExpress(app);
 configHandlebars(app);
 
-app.get("/", (req, res) => {
-    res.status(200).write("<h1>Hello</h1>");
-});
+app.use(routes);
 
 app.listen(port, () => console.log("Server is listening on port 3000..."));
