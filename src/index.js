7a1b7e96df182e1042af95f44670cf3ced1746a1
Nick <nkmitkov@gmail.com>
2024-06-11 08:35:30 +0300

    Add express-handlebars and config server

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 
+const configExpress = require("./config/configExpress");
+const configHandlebars = require("./config/configHandlebars");
+
 const app = express();
 const port = 3000;
+
+configExpress(app);
+configHandlebars(app);
 
 app.get("/", (req, res) => {
     res.status(200).write("<h1>Hello</h1>");
