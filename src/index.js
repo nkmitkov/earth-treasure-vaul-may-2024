bb3614798bd47963d4e1dbf6f9335a643e65c974
Nick <nkmitkov@gmail.com>
2024-06-11 08:22:57 +0300

    Add express lib and create server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.status(200).write("<h1>Hello</h1>");
+});
+
+app.listen(port, () => console.log("Server is listening on port 3000..."));
